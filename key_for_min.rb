f968b16df050ad49840c6f9367439e09a3eae338
Deborah Santillan <s.debb@yahoo.com>
2020-06-20 17:58:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-animal_weight = {:lion => 300, :hippo => 1000, jiraffe => 200}
+
 def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
